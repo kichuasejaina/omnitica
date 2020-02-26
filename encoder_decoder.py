a86bae15e7de4de4693584f2a324b66a9a5ec253
warrior <warrior.momo.com>
2020-02-26 16:41:33 +0530

    Adding Encoder

---
+++ encoder_decoder.py
@@ -0,0 +1,30 @@
+from cryptography.fernet import Fernet
+
+
+def encoder():
+    key = Fernet.generate_key()
+    encrypter = Fernet(key)
+    length_of_key = len(key)
+    user_input1 = input("Please enter the string to encode: ")
+    if user_input1.strip('\n').strip('\r') == '':
+        print("Need to provide string to encode. Exiting!!!!!!")
+    else:
+        user_input2 = input("Please confirm the string to encode: ")
+        if (user_input1 != user_input2):
+            print("String mismatch... Exiting!!")
+        else:
+            user_input = user_input1.strip('\n').strip('\r')
+            encrypted_data = encrypter.encrypt(user_input.encode()).decode()
+            print("{}#{}{}".format(length_of_key, key.decode(), encrypted_data))
+
+
+def decoder(encoded_string):
+    keylength = int(encoded_string.split('#')[0])
+    data = "#".join(encoded_string.split('#')[1:])
+    key = data[:keylength].encode()
+    encrypted_data = data[keylength:].encode()
+    decrypter = Fernet(key)
+    decrypted_data = decrypter.decrypt(encrypted_data).decode()
+    return decrypted_data
+
+##
