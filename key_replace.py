7bb98f66e0a692129b5d286514f8bcd36bec5f39
kichuasejaina <kichuasejaina@gmail.com>
2020-05-06 23:50:54 +0530

    Update key_replace.py

--- key_replace.py
+++ key_replace.py
@@ -51,6 +51,7 @@ def get_dictionary_from_keys_file(key_map_file):
         return key_map_data
     except Exception as ex:
         print("!!!!! Failed to check key_map file. {} !!!!!".format(ex.args))
+        exit(1)
 
 
 def replace_file(file_location, key_map_data):
